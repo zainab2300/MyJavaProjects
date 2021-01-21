a4aae81b5ae7799f0ed151631047c78d6e8c64dc
Admin <Admin@10.0.0.18>
2021-01-20 21:07:48 -0500

    My second test

--- src/com/january2021/calculator/MyCalculator.java
+++ src/com/january2021/calculator/MyCalculator.java
@@ -3,7 +3,7 @@ package com.january2021.calculator;
 public class MyCalculator {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello, how are you");
 
 	}
 
