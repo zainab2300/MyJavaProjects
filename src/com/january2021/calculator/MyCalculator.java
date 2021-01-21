088da073c5a4887c86471974c5e7cc9dbfa45f09
Admin <Admin@10.0.0.18>
2021-01-20 21:04:58 -0500

    My first commit

--- src/com/january2021/calculator/MyCalculator.java
+++ src/com/january2021/calculator/MyCalculator.java
@@ -3,7 +3,7 @@ package com.january2021.calculator;
 public class MyCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello");
 
 	}
 
